6c8fe31e15410af2f8b56ea9e68bfede870d4a9c
sujesh <sujeshjsvtra@gmail.com>
2024-02-14 22:24:55 +0530

    Initial commit

---
+++ src/main/java/com/tawasul/web/resource/LoginModel.java
@@ -0,0 +1,40 @@
+package com.tawasul.web.resource;
+
+public class LoginModel {
+
+	private String userName;
+	private String password;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LoginModel(String userName, String password) {
+		super();
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public LoginModel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "LoginModel [userName=" + userName + ", password=" + password + "]";
+	}
+
+}
---
+++ src/main/java/com/tawasul/web/service/AuthenticateService.java
@@ -0,0 +1,5 @@
+package com.tawasul.web.service;
+
+public interface AuthenticateService {
+
+}
