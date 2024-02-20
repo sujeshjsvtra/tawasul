9f85d5b6580d481a093c000fe27a50ed1080a69c
Vinay <vin070@gmail.com>
2024-02-20 13:21:27 +0530

    update schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
-CREATE TABLE Users
+CREATE TABLE User
 (
     id    INT          NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name  VARCHAR(50)  NOT NULL,
+    name  VARCHAR(100)  NOT NULL,
     email VARCHAR(100) NOT NULL,
     role  VARCHAR(50),
     created_at   TIMESTAMP,
@@ -9,11 +9,12 @@ CREATE TABLE Users
 
 );
 
-CREATE TABLE Sectors
+CREATE TABLE Sector
 (
     id    INT          NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     sector_id   INT         NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    sector_name VARCHAR(50) NOT NULL,
+    sector_name VARCHAR(100) NOT NULL,
+    sector_name_arabic VARCHAR(100) NOT NULL,
     created_at   TIMESTAMP,
     status VARCHAR(2),
     user_id INT
