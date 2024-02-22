f152e38bc3df3f7829c7316c70b36c7ae47e4e1f
Vinay <vin070@gmail.com>
2024-02-22 11:01:48 +0530

    add role entity

---
+++ src/main/java/com/tawasul/web/model/Role.java
@@ -0,0 +1,37 @@
+package com.tawasul.web.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "role", schema = "public")
+public class Role {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+
+	@CreationTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created_at")
+	private Date createdAt;
+
+	@UpdateTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated_at")
+	private Date updatedAt;
+
+	private String status;
+}
