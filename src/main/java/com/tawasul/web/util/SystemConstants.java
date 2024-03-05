b8bb9b49d47b1a21c306db4a26c06ae117b83482
Vinay <vin070@gmail.com>
2024-03-05 23:23:15 +0530

    fix conflicts

--- src/main/java/com/tawasul/web/util/SystemConstants.java
+++ src/main/java/com/tawasul/web/util/SystemConstants.java
@@ -9,22 +9,21 @@ public class SystemConstants {
 	public static final String LOGIN_SCREEN="login.xhtml";
 	public static final String VIEW_SECTOR_SCREEN="view-sector.xhtml";
 	public static final String ADD_SECTOR_SCREEN="add-sector.xhtml";
-	public static final String VIEW_CONSULTATIONS_SCREEN="view-consultation.xhtml";
+	public static final String VIEW_CONSULTATIONS_SCREEN="view-consultations.xhtml";
 	public static final String ADD_CONSULTATIONS_SCREEN="add-consultation.xhtml";
 	public static final String VIEW_INITIATIVES_SCREEN="initiatives.xhtml";
 	
 	public static final String DASHBOARD_MENU="admin-dashboard";
 	public static final String VIEW_SECTOR_MENU="view-sector";
 	public static final String ADD_SECTOR_MENU="add-sector";
-	public static final String VIEW_CONSULTATIONS_MENU="view-consultation";
-	public static final String ADD_CONSULTATIONS_MENU="add-consultation";
+	public static final String VIEW_CONSULTATIONS_MENU="view-consultations";
+	public static final String ADD_CONSULTATIONS_MENU="add-consultations";
 	public static final String VIEW_INITIATIVES_MENU="initiatives";
 	
 	public static final String LABEL_DASHBOARD="Dashboard";
 	public static final String LABEL_VIEW_SECTOR="View Sector";
 	public static final String LABEL_ADD_SECTOR="Add Sector";
-	public static final String LABEL_ADD_CONSULTATIONS="Add Consultation";
-	public static final String LABEL_EDIT_CONSULTATIONS="Edit Consultation";
+	public static final String LABEL_ADD_CONSULTATIONS="Add Consultations";
 	public static final String LABEL_VIEW_CONSULTATIONS="View Consultations";
 	public static final String LABEL_VIEW_INITIATIVES="Initiatives";
 	
@@ -34,7 +33,6 @@ public class SystemConstants {
 
 	public static final int ACTIVE = 1;
 	public static final int IN_ACTIVE = 0;
-
 
 
 	public static final String CONSULTATION_MODULE_NAME = "CONSULTATION";
