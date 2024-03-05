4e180fae4e7bc9de4a485305402de7ac67466d9b
sujeshjsvtra <sujeshjsvtra@gmail.com>
2024-03-05 22:50:08 +0530

    Update AuthenticationService.java

--- src/main/java/com/tawasul/web/service/AuthenticationService.java
+++ src/main/java/com/tawasul/web/service/AuthenticationService.java
@@ -6,5 +6,7 @@ import com.tawasul.web.model.User;
 public interface AuthenticationService {
 	
 	public User login(LoginModel loginModel)throws Exception;
+	
+	public void saveOtp(String email,String otp);
 
 }
